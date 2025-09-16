881102317efa3289f7325e7564bf66a8512d60d9
sakura <165626344+sakuranadira@users.noreply.github.com>
2025-09-16 23:12:48 +0700

    Commit No. 1 – add fragment lifecyle logging

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lab_week_03
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v,
+                                                                             insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right,
+                systemBars.bottom)
             insets
         }
+
+        Log.d(TAG, "onCreate")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+    companion object {
     }
 }
+private const val TAG = "MainActivity"
